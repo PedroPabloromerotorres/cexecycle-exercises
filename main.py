df773b04638de8a94bdb39629534e156dc24ad1c
PedroPabloromerotorres <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-10-31 13:18:31 -0500

    solucion del ejercicio suma entre numeros

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-#Escriba un programa que genere todas las potencias de 2, desde la 0-ésima hasta la ingresada por el usuario:
+#Escriba un programa que pida al usuario dos números enteros, y luego entregue la suma de todos los números que están entre ellos. Por ejemplo, si los números son 1 y 7, debe entregar como resultado 2 + 3 + 4 + 5 + 6 = 20.
 
-#Ingrese num: 10
-#1 2 4 8 16 32 64 128 256 512 1024
+#Ingrese num: 1
+#Ingrese num: 7
+#La suma es 20
 
-num = int(input("Ingrese un número: "))
+num1 = int(input("Ingrese el primer número: "))
 
-potencias = [2**i for i in range(num + 1)]
+num2 = int(input("Ingrese el segundo número: "))
 
-print(" ".join(map(str, potencias)))
+inicio = min(num1, num2) + 1
+
+fin = max(num1, num2)
+
+suma = sum(range(inicio, fin))
+
+print(f"La suma es {suma}")
