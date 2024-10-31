5aa63e28fcd6630ba4312a79429812686c1a632b
PedroPabloromerotorres <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-10-31 13:34:49 -0500

    solucion del ejercicio suma de fracciones

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-#esarolle un programa para estimar el valor de π usando la siguiente suma infinita:
+#Desarrolle un programa que permita trabajar con las potencias fraccionales de dos, es decir:
 
-#π=4(1−13+15−17+…)
-#La entrada del programa debe ser un número entero n
-# que indique cuántos términos de la suma se utilizará.
+#12,14,18,116,132,164,…
+#en forma decimal:
 
-#n: 3
-#3.466666666666667
-#n: 1000
-#3.140592653839794
+#0.5,0.25,0.125,0.0625,0.03125,0.015625,…
+#El programa debe mostrar tres columnas que contengan la siguiente información:
 
-n = int(input("n: "))
+#Potencia  Fraccion  Suma
+#1         0.5       0.5
+#2         0.25      0.75
+#3         0.125     0.875
+#4         0.0625    0.9375
+#...       ...       ...
+#El programa debe terminar cuando la fracción decimal sea menor o igual a 0.000001.
 
+potencia = 1
+fraccion = 0.5
 suma = 0
 
-for k in range(n):
+print(f"{'Potencia':<10} {'Fracción':<10} {'Suma':<10}")
+
+while fraccion > 0.000001:
+    suma += fraccion
+    print(f"{potencia:<10} {fraccion:<10.6f} {suma:<10.6f}")
     
-    termino = ((-1) ** k) / (2 * k + 1)
-
-    suma += termino
-
-pi_estimado = 4 * suma
-
-print(pi_estimado)
+    potencia += 1
+    fraccion /= 2 
